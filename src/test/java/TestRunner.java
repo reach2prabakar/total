7d7a48e5d5fb413866e66d902f6b5f9dfeac07e6
prabha <prabs.nz@gmail.com>
2022-06-03 12:42:26 +1200

    report path updated to previous version

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -5,9 +5,9 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        plugin = {"pretty","html:target/cucumber-pretty",
-                "json:targe/CucumberTestReport.json",
-                "rerun:target/rerun.txt" },
+        plugin = {"pretty","html:target/cucumber-reports/cucumber-pretty",
+                "json:target/cucumber-reports/CucumberTestReport.json",
+                "rerun:target/cucumber-reports/rerun.txt" },
         monochrome = true,
         glue = {
                 "com.toptal.stepdefinitions"
